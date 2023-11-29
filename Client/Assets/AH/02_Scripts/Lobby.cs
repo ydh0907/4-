ec0a97786848e0883033176418d3659cb27fe7c1
ahreum0408 <ahreun0408@gmail.com>
2023-11-29 19:18:58 +0900

    Revert "lobby basic finish"
    This reverts commit 7ddefab96e96c5972e3e7e5479bd43e04301d091.

--- Client/Assets/AH/02_Scripts/Lobby.cs
+++ Client/Assets/AH/02_Scripts/Lobby.cs
@@ -12,13 +12,13 @@ namespace AH {
 
         private Label _waringTxt;
 
-        //private Button _createGame;
-        //private Button _enterGame;
-        //private Button _settingWindow;
-        //private Button _exit;
+        private Button _createGame;
+        private Button _enterGame;
+        private Button _settingWindow;
+        private Button _exit;
 
         private string _joincode;
-        [SerializeField] private string _inputCode;
+        [SerializeField] private string _testCode;
 
 
         private void Awake() {
@@ -31,41 +31,36 @@ namespace AH {
             _waringTxt = root.Q<Label>("waring-txt");
 
             _inputJoinCode.RegisterCallback<ChangeEvent<string>>(HandleJoinCodeChanged); // 입력하는 값이 변경될 때마다 호출됨
-
             root.Q<Button>("create-btn").RegisterCallback<ClickEvent>(HandleCreateRoom);
             root.Q<Button>("enter-btn").RegisterCallback<ClickEvent>(HandleEnterRoom);
             root.Q<Button>("setting-btn").RegisterCallback<ClickEvent>(OpenSettingWindow);
             root.Q<Button>("exit-btn").RegisterCallback<ClickEvent>(ExitGame);
         }
 
-        /// <summary>
-        /// input joincode(값이 변경될 때마다 호출됨
-        /// </summary>
-        /// <param name="evt"></param>
-        private void HandleJoinCodeChanged(ChangeEvent<string> evt) {
+        private void HandleJoinCodeChanged(ChangeEvent<string> evt) { // input joincode
             _joincode = evt.newValue;
         }
         private void HandleCreateRoom(ClickEvent evt) { // host
-            Debug.Log("CREATE ROOM");
+            Debug.Log("create");
         }
         private void HandleEnterRoom(ClickEvent evt) { // client
-            if(_joincode != null && _joincode == _inputCode) { // 여기에 서버에서 받은 joincode 값을 비교해서 들어갈 수 있게 해준다
-                Debug.Log("ENTER ROOM");
+            if(_joincode != null && _joincode == _testCode) { // 여기에 서버에서 받은 joincode 값을 비교해서 들어갈 수 있게 해준다
+                Debug.Log("enter");
             }
             else {
                 StartCoroutine(DisplayWaring());
+                Debug.Log("값을 다시 입력해주세요..");
             }
         }
         private void OpenSettingWindow(ClickEvent evt) { // setting
-            Debug.Log("SETTING");
+            Debug.Log("setting");
         }
         private void ExitGame(ClickEvent evt) { // exit
-            Debug.Log("EXIT");
             Application.Quit();
         }
 
         IEnumerator DisplayWaring() {
-            _waringTxt.text = "코드가 맞지 않습니다.\n다시 작성해주세요";
+            _waringTxt.text = "코드가 맞지 않습니다. 다시 작성해주세요";
             yield return new WaitForSeconds(2f);
             _waringTxt.text = "";
             yield return null;
