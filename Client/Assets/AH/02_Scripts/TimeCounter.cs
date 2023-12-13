da21be74e7c6641029120756918da1e2e41aea50
ahreum0408 <ahreun0408@gmail.com>
2023-12-13 20:04:11 +0900

    finish countdown
    lobby는 하는 중

--- Client/Assets/AH/02_Scripts/TimeCounter.cs
+++ Client/Assets/AH/02_Scripts/TimeCounter.cs
@@ -4,24 +4,35 @@ using UnityEngine.UIElements;
 
 namespace AH {
     public class TimeCounter : MonoBehaviour {
+        IngameUIToolkit ingameToolkit;
+
+        private void Awake() {
+            ingameToolkit = GetComponent<IngameUIToolkit>();
+        }
+
         public void CountDown(Label countText) {
             int startCount = Random.Range(3, 11);
 
             countText.text = startCount.ToString(); // 시작값을 바꾸고
-            StartCoroutine(RoutineCountDown(countText, startCount));
+            StartCoroutine(RoutineCountDown(countText, startCount, 4));
         }
-        IEnumerator RoutineCountDown(Label countText, int time) {
-            int count = 4;
-            while(count > 0) {
-                Debug.Log(count);
-                countText.text = time.ToString();
-                /*if(time <= 0){
-                    break;
-                } */
-                count--;
+        public void ResurrectionCountDown(Label countText) {
+            int startCount = 3;
+
+            countText.text = $"{startCount}초 뒤 부활"; // 시작값을 바꾸고
+            StartCoroutine(RoutineCountDown(countText, startCount, 4, "초 뒤 부활"));
+        }
+
+        IEnumerator RoutineCountDown(Label countText, int time, int loopTime, string plusText = "") {
+            while(loopTime > 0) {
+                //Debug.Log(loopTime);
+                countText.text = $"{time}{plusText}";
+
+                loopTime--;
                 time--;
                 yield return new WaitForSeconds(1);
             }
+            ingameToolkit.FinishCountDown();
         }
     }
 }
