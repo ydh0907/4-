a5217962b42e9544eb5f7e51e3b0a3d17a21e69e
kimhaneun <kimhaneun070929@gmail.com>
2023-12-21 01:11:33 +0900

    Playr Attack St
    만ㅡ는 중이양~~

--- Client/Assets/HE/1. Scripts/1.0 Player/PlayerAttack.cs
+++ Client/Assets/HE/1. Scripts/1.0 Player/PlayerAttack.cs
@@ -4,41 +4,55 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // PlayerKnockback.StartKnockback(hitDirection, hitDirection, PlayerMovement._moveInput.x);
-
-    public int _attackDamageAmount;
-    // 나중에 공격 시간 등도 설정 해 줘야 할거야..
-
+    #region COMPONENTS
     private Animator Animator;
+    #endregion
 
     private PlayerDamageble PlayerDamageble;
 
-    public bool IsAttacking { get; private set; }
+    #region STATE PARAMETERS
+    private bool _attackRefilling;
+
+    [SerializeField] private float _attackRefillTime;
+    [SerializeField] private float _attackSpeed;
+    #endregion
 
     private void Awake()
     {
         Animator = GetComponent<Animator>();
+
+        PlayerDamageble = GetComponent<PlayerDamageble>();
     }
 
     private void Update()
     {
+        #region INPUT HANDLER
         // attack
         if (CanAttack() && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            IsAttacking = true;
-
             Attack();
         }
+        #endregion
+
+        Animator.SetFloat("AttackSpeed", _attackSpeed);
     }
 
     private void Attack()
     {
-        Animator.SetTrigger("");
+        StartCoroutine(nameof(RefillAttack));
+        Animator.SetTrigger("isAttacking");
+    }
+
+    public IEnumerator RefillAttack()
+    {
+        _attackRefilling = true;
+        yield return new WaitForSeconds(_attackRefillTime);
+        _attackRefilling = false;
     }
 
     private bool CanAttack()
     {
-        if (!PlayerDamageble.IsGroggying)
+        if (!PlayerDamageble.IsGroggying && !_attackRefilling)
             return true;
         else
             return false;
