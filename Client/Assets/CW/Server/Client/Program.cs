87d0f7afa09fe3b80719c683188ffc182e7be6b6
Karin <karin.cw07@gmail.com>
2023-12-20 18:04:07 +0900

    테스트함

--- Client/Assets/CW/Server/Client/Program.cs
+++ Client/Assets/CW/Server/Client/Program.cs
@@ -1,15 +1,17 @@
 ﻿using Karin.Network;
 using Packets;
 using System.Net;
+using UnityEngine;
 
 namespace TestClient
 {
-    internal class Program
+    internal class Program : MonoBehaviour
     {
         public static Connector connector;
         public static ServerSession serverSession;
 
-        static void Main(string[] args)
+        [ContextMenu("Server")]
+        private async void ServerConnect()
         {
             IPAddress ipAddress = IPAddress.Parse("172.31.3.130");
             IPEndPoint endPoint = new IPEndPoint(ipAddress, 8081);
@@ -18,11 +20,11 @@ namespace TestClient
             connector = new Connector(endPoint, serverSession);
             connector.StartConnect(endPoint);
 
-            while (true)
-            {
+            //while (true)
+            //{
 
 
-            }
+            //}
 
         }
 
