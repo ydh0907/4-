be367ff6d1822412f26c7dddc17872d45e353036
Yundohee <ydohee0907@gmail.com>
2023-12-21 01:26:04 +0900

    동기화

--- Client/Assets/CW/Server/Client/PacketHandler.cs
+++ Client/Assets/CW/Server/Client/PacketHandler.cs
@@ -9,15 +9,14 @@ namespace TestClient
         public static void C_RoomCreatePacket(Session session, Packet packet)
         {
             C_RoomCreatePacket c_RoomCreatePacket = packet as C_RoomCreatePacket;
-            // 방만들기 완료
             Program.Instance.DisConnectServer();
         }
 
         public static void C_ReRoadingPacket(Session session, Packet packet)
         {
             C_ReRoadingPacket c_ReRoadingPacket = packet as C_ReRoadingPacket;
-            // 방정보 공유 .Rooms
             Program.Rooms = c_ReRoadingPacket.Rooms;
+            Program.Instance.reload = true;
             Program.Instance.DisConnectServer();
         }
     }
