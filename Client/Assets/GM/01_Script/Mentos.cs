c29db1a0f45da345a7f7bbf64402b227fe428757
sgm0256 <gubbojoky0526@gmail.com>
2023-12-22 00:29:18 +0900

    mentos

---
+++ Client/Assets/GM/01_Script/Mentos.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GM
+{
+    public class Mentos : MonoBehaviour
+    {
+        private bool isUp = true;
+        private void Update()
+        {
+            if (transform.position.y >= 0.7f)
+            {
+                isUp = false;
+            }
+            else if (transform.position.y <= 0.4f)
+            {
+                isUp = true;
+            }
+
+            Vector3 pos = transform.position;
+            if (isUp)
+            {
+                pos.y += Time.deltaTime / 5;
+            }
+            else
+            {
+                pos.y -= Time.deltaTime / 5;
+            }
+            transform.position = pos;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.name.Contains("Player"))
+            {
+                Destroy(gameObject);
+                Debug.Log("맨토스 수집됨");
+            }
+        }
+    }
+}
