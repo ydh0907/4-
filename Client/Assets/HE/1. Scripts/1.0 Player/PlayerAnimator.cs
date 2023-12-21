623d82ca4233a9709b65a8fea8fd761adf3e7991
kimhaneun <kimhaneun070929@gmail.com>
2023-12-21 20:22:53 +0900

    playr movmnt 오류 해결 과정

--- Client/Assets/HE/1. Scripts/1.0 Player/PlayerAnimator.cs
+++ Client/Assets/HE/1. Scripts/1.0 Player/PlayerAnimator.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+namespace HE
 {
-    private PlayerMovement PlayerMovement;
-
-    private Animator Animator;
-
-    private void Awake()
+    public class PlayerAnimator : MonoBehaviour
     {
-        PlayerMovement = GetComponent<PlayerMovement>();
+        private PlayerMovement PlayerMovement;
 
-        Animator = GetComponent<Animator>();
-    }
+        private Animator Animator;
 
-    private void LateUpdate()
-    {
-        // 애니메이션이 중간에 끊기면 안 되는건 트리거 예)공격 점프, 교체 애니메이션??
-        // 중간에 끊겨도 되는거면 bool 예) 런, 러쉬
+        private void Awake()
+        {
+            PlayerMovement = GetComponent<PlayerMovement>();
+
+            Animator = GetComponent<Animator>();
+        }
+
+        private void LateUpdate()
+        {
+            // 애니메이션이 중간에 끊기면 안 되는건 트리거 예)공격 점프, 교체 애니메이션??
+            // 중간에 끊겨도 되는거면 bool 예) 런, 러쉬
+        }
     }
 }
--- Client/Assets/HE/1. Scripts/1.0 Player/PlayerAttack.cs
+++ Client/Assets/HE/1. Scripts/1.0 Player/PlayerAttack.cs
@@ -2,59 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
+namespace HE
 {
-    #region COMPONENTS
-    private Animator Animator;
-    #endregion
 
-    private PlayerDamageble PlayerDamageble;
-
-    #region STATE PARAMETERS
-    private bool _attackRefilling;
-
-    [SerializeField] private float _attackRefillTime;
-    [SerializeField] private float _attackSpeed;
-    #endregion
-
-    private void Awake()
+    public class PlayerAttack : MonoBehaviour
     {
-        Animator = GetComponent<Animator>();
-
-        PlayerDamageble = GetComponent<PlayerDamageble>();
-    }
-
-    private void Update()
-    {
-        #region INPUT HANDLER
-        // attack
-        if (CanAttack() && Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Attack();
-        }
+        #region COMPONENTS
+        private Animator Animator;
         #endregion
 
-        Animator.SetFloat("AttackSpeed", _attackSpeed);
-    }
+        private PlayerDamageble PlayerDamageble;
 
-    private void Attack()
-    {
-        StartCoroutine(nameof(RefillAttack));
-        Animator.SetTrigger("isAttacking");
-    }
+        #region STATE PARAMETERS
+        private bool _attackRefilling;
 
-    public IEnumerator RefillAttack()
-    {
-        _attackRefilling = true;
-        yield return new WaitForSeconds(_attackRefillTime);
-        _attackRefilling = false;
-    }
+        [SerializeField] private float _attackRefillTime;
+        [SerializeField] private float _attackSpeed;
+        #endregion
 
-    private bool CanAttack()
-    {
-        if (!PlayerDamageble.IsGroggying && !_attackRefilling)
-            return true;
-        else
-            return false;
+        private void Awake()
+        {
+            Animator = GetComponent<Animator>();
+
+            PlayerDamageble = GetComponent<PlayerDamageble>();
+        }
+
+        private void Update()
+        {
+            #region INPUT HANDLER
+            // attack
+            if (CanAttack() && Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Attack();
+            }
+            #endregion
+
+            Animator.SetFloat("AttackSpeed", _attackSpeed);
+        }
+
+        private void Attack()
+        {
+            StartCoroutine(nameof(RefillAttack));
+            Animator.SetTrigger("isAttacking");
+        }
+
+        public IEnumerator RefillAttack()
+        {
+            _attackRefilling = true;
+            yield return new WaitForSeconds(_attackRefillTime);
+            _attackRefilling = false;
+        }
+
+        private bool CanAttack()
+        {
+            if (!PlayerDamageble.IsGroggying && !_attackRefilling)
+                return true;
+            else
+                return false;
+        }
     }
 }
