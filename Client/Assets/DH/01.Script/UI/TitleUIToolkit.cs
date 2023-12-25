940f32a094a9dcbd34901c26ecc414e168c69d6a
ahreum0408 <ahreun0408@gmail.com>
2023-12-25 23:45:53 +0900

    sound
    스크립트만 있음 uiX

--- Client/Assets/DH/01.Script/UI/TitleUIToolkit.cs
+++ Client/Assets/DH/01.Script/UI/TitleUIToolkit.cs
@@ -1,4 +1,6 @@
+using Karin;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -6,12 +8,15 @@ using UnityEngine.UIElements;
 namespace DH {
     public class TitleUIToolkit : MonoBehaviour {
         private UIDocument _uiDocument;
+        private VisualElement root;
+
+        [SerializeField] private VisualTreeAsset _settingPanel;
 
         private void Awake() {
             _uiDocument = GetComponent<UIDocument>();
         }
         private void OnEnable() {
-            var root = _uiDocument.rootVisualElement;
+            root = _uiDocument.rootVisualElement;
 
             root.Q<Button>("playgame-btn").RegisterCallback<ClickEvent>(HandleInputPlayerDataScene);
             root.Q<Button>("setting-btn").RegisterCallback<ClickEvent>(OpenSettingWindow);
@@ -22,8 +27,51 @@ namespace DH {
             SceneManager.LoadScene("DH_Lobby");
         }
         private void OpenSettingWindow(ClickEvent evt) { // setting
-            Debug.Log("SETTING");
+            var template = _settingPanel.Instantiate().Q<VisualElement>("setting-border");
+            var bgmData = template.Q<VisualElement>("bgm-content");
+            var effectData = template.Q<VisualElement>("effect-content");
+
+            List<VisualElement> bgmList = new List<VisualElement>();
+            List<VisualElement> effectList = new List<VisualElement>();
+
+            //GetSoundVisualElementData(bgmList, bgmData); // 생성할 때마다 가져와야 함
+            //GetSoundVisualElementData(effectList, effectData);
+            //GetcurrentSoundData(bgmList, effectList);
+
+            root.Clear();
+            root.Add(template);
+
+            template.AddToClassList("on");
+
+            template.Q<Button>("close-btn").RegisterCallback<ClickEvent>(HandleCloseButton);
+
+            VisualElement bgmValueButton = template.Q<VisualElement>(className: "bgm-content");
+            VisualElement effectValueButton = template.Q<VisualElement>(className: "effect-content");
+            bgmValueButton.RegisterCallback<ClickEvent>(evt => {
+                var btn = evt.target as DataSound;
+                if (btn != null) {
+                    int index = bgmList.IndexOf(btn);
+
+                    //SoundManager.Instance.bgmValue = index;
+                    //SoundManager.Instance.RegulateSound(Sound.Bgm, index);
+                    OnOffImages(bgmList, index);
+                }
+            });
+            effectValueButton.RegisterCallback<ClickEvent>(evt => {
+                var btn = evt.target as DataSound;
+                if (btn != null) {
+                    int index = effectList.IndexOf(btn);
+
+                    //SoundManager.Instance.effectValue = index;
+                    //SoundManager.Instance.RegulateSound(Sound.Effect, index);
+                    OnOffImages(effectList, index);
+                }
+            });
         }
+        private void HandleCloseButton(ClickEvent evt) {
+
+        }
+
         private void ExitGame(ClickEvent evt) { // exit
             Application.Quit();
             Debug.Log("QUIT");
